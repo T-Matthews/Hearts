d6585b9f0ec774f092f4c60cade6f7a55e58d864
Zach Loza <Zach10za@users.noreply.github.com>
2022-08-31 15:22:11 -0700

    Handle creating Players and persisting in browser

--- hearts/views.py
+++ hearts/views.py
@@ -1,22 +1,15 @@
 from uuid import uuid4
 
-from django.http import HttpResponseRedirect
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.generic import FormView, TemplateView
 
+from hearts.constants import PLAYER_ID_COOKIE_KEY
+from hearts.decorators import require_player
 from hearts.forms import NewPlayerForm
 from hearts.models import Game, Player
-
-
-class HomeTemplateView(TemplateView):
-    """
-    Home page.
-
-    Should direct user to server browser or create new game.
-    """
-
-    template_name = 'hearts/home.html'
 
 
 class NewPlayerFormView(FormView):
@@ -32,16 +25,39 @@ class NewPlayerFormView(FormView):
     success_url = reverse_lazy('home')
 
     def form_valid(self, form: NewPlayerForm) -> HttpResponseRedirect:
-        player = self.create_player(form.name)
-        # TODO: Set cookie with player id.
-        return super().form_valid(form)
+        response = super().form_valid(form)
+        player = self.create_player(form.cleaned_data['name'])
+        response.set_cookie(PLAYER_ID_COOKIE_KEY, player.id)
+        return response
 
-    def create_player(self, name: str) -> Player:
+    @staticmethod
+    def create_player(name: str) -> Player:
         """
         Create a new Player.
         """
+        player = Player.objects.create(name=name)
+        return player
 
 
+@method_decorator(require_player, name='dispatch')
+class HomeTemplateView(TemplateView):
+    """
+    Home page.
+
+    Should direct user to server browser or create new game.
+    """
+
+    template_name = 'hearts/home.html'
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'player': self.request.player,
+        })
+        return context
+
+
+@method_decorator(require_player, name='dispatch')
 class GameBrowserTemplateView(TemplateView):
     """
     Render list of available games.
@@ -66,12 +82,13 @@ class GameBrowserTemplateView(TemplateView):
         return context
 
 
+@method_decorator(require_player, name='dispatch')
 class NewGameView(View):
     """
     View to handle creation of games.
     """
 
-    def get(self, request) -> HttpResponseRedirect:
+    def get(self, request: HttpRequest) -> HttpResponse:
         """
         Handle simple GET request which will create a game and redirect to it.
         """
@@ -88,6 +105,7 @@ class NewGameView(View):
         """
 
 
+@method_decorator(require_player, name='dispatch')
 class GameTemplateView(TemplateView):
     """
     Main view for the actual game.
